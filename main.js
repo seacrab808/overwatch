2c8523d152b809a1ecb9e30c19387ed0e5d280bc
yuna <seacrab808@naver.com>
2023-07-23 16:16:36 +0900

    상단바 디자인

--- main.js
+++ main.js
@@ -4,12 +4,11 @@ dropdowns.forEach( dropdown => {
   const select = dropdown.querySelector('.select');
   const caret = dropdown.querySelector('.caret');
   const menu = dropdown.querySelector('.menu');
-  const options = dropdown.querySelectorAll('.menu li');
-  const selected = dropdown.querySelector('.selected');
 
-  select.addEventListener('active', () => {
+  select.addEventListener('mouseover', () => {
     select.classList.toggle('select-clicked');
     caret.classList.toggle('caret-rotate');
     menu.classList.toggle('menu-open');
   })
+
 });
